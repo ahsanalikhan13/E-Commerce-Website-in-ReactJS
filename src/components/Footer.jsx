dc708f93788f7f5776c733619ccad307aa34b232
ahsanalikhan13 <aahsanalikhan.13@gmail.com>
2023-02-02 18:15:15 +0500

    third commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return(
+    <footer>
+        <div>
+            <h1>Disclaimer</h1>
+            <p>@all rights reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <a href='https://www.youtube.com/'target={"blank"}>Youtube</a>
+            <a href='https://www.google.com/'target={"blank"}>Google</a>
+            <a href='https://www.instagram.com/'target={"blank"}>Instagram</a>
+
+        </div>
+    </footer>
+    )
+}
+
+export default Footer
